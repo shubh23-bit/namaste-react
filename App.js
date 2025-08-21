1c3ca294109825246b77a9c04f80ae64443ab049
Shubham Bisht <shubhambisht758@gmail.com>
2025-08-21 11:09:52 +0530

    coponent

--- App.js
+++ App.js
@@ -1,26 +1,42 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-{/* <div>
-    <div>
-        <h1>i am h1 tag</h1>
-    </div>
-</div> */}
+//element 
+const heading=React.createElement("h1",{id:"heading"},"namste React")
+console.log(heading)
+const root=ReactDOM.createRoot(document.getElementById("root"));
 
-//this how create nested html tag in react 
-const parent=React.createElement(
-    "div",
-    {id:"parent"},
-    React.createElement("div",{id:"child"},
-    React.createElement("h1",{},"i am h tag"))
-)
+//jsx=>it is html-like or xml-like sysntax
+//babael is converted to jsx to react element
+const jsxHeading=<h1 className="head">namste lwc</h1>
+//given attribute to jsx its camelcase
+//react component=>there are to type of componenet=>classBased componenet=>functional componenet
+//Functional component=>is javascript function return a jsx 
+const elem=<span>React Element</span>
+const Title=()=>{
+    {elem}
+    return(
+        <h1>this tiltile function componeent</h1> 
+        
+    )
+}
+//componenet composition 
+const number=10000
+    const HeadingComponent=()=>{//this is functonal component
+        {elem}
+        return(
+            <> 
+            {/* componenet compositionone componenet into another component is componenet compostion */}
+            <Title/>
+            <Title/>
+            
+            <div id="container">
+                <h2>{number+number}</h2>
 
+            </div>
+            <h1>lwc functional  copnenet </h1>
+            </>
+        )
+    }
 
-
-const heading=React.createElement(
-    "h1",
-    {id:"heading"},
-    "hello shubham from react"
-)
-console.log(parent)
-const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent)
-
+root.render(<HeadingComponent/>)
